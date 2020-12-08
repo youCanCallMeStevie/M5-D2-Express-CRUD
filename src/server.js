77337a024ef1f2ba984daa23f75e26cf5b6e6bd1
youCanCallMeStevie <72197493+youCanCallMeStevie@users.noreply.github.com>
2020-12-08 11:41:45 +0000

    first days of backend, notes on API crud

--- src/server.js
+++ src/server.js
@@ -1,14 +1,19 @@
 const express = require("express")
+const userRoutes = require("./users") //automatically with grab index file in that folder
+
+const server = express()
+const port = 3001
+
+
 const cors = require("cors")
 
 const users = require("./services/users")
 
-const server = express()
 
 server.use(cors())
 server.use(express.json())
 
-server.use("/users", users)
-server.listen(3001, () => {
-  console.log("Server running on port 3001")
+server.use("/users", userRoutes)
+server.listen(port, () => {
+  console.log("Server running on port:", port)
 })
--- src/services/users/index.js
+++ src/services/users/index.js
@@ -1,58 +1,113 @@
-const express = require("express")
-const fs = require("fs")
-const path = require("path")
-const uniqid = require("uniqid")
+/* 
+1. get all users on url -> localhost:3001/users
+2. get single user with id on url -> localhost:3001/users/:id
+3. create a single user -> localhost:3001/users
+4. modify a single user -> localhost:3001/users/:id
+5. delete a single user -> localhost:3001/users/:id
+All the routes in this file will have the /users/ prefix
+*/
 
-const router = express.Router()
+const express = require("express");
+const router = express.Router();
+// router.get("/") router.post("/:id") can create the whole collection with this router & can organise all of our "things" with this router
+const fs = require("fs"); //core module
+const path = require("path"); //core module
+const uniqid = require("uniqid"); //third party module, it has to be installed. this will produce unique ids for us
 
+//1. router.get("/")
 router.get("/", (req, res) => {
-  const buffer = fs.readFileSync(path.join(__dirname, "users.json"))
-  const fileContent = buffer.toString()
+  //handler, special function that takes care of understanding what was the request
+  const usersFilePath = path.json(__dirname, "users.json");
+  //a) retrive the list fro a file on disk (users.json), because we don't havea  real database yet
+  const fileAsABuffer = fs.readFileSync(usersFilePath); //returns a buffer, machine readbale, not human readable!
+  //b) we get a bugger so it must be converted to something readable, like a string
+  const fileAsAString = fileAsABuffer.toString();
+  //c) I want to send th list as a response but in the form of a JSON, not a string
+  const usersArray = JSON.parse(fileAsAString)
+  res.send(usersArray);
+});
 
-  res.send(JSON.parse(fileContent))
-})
-
+//2. router.get("/:id")
 router.get("/:id", (req, res) => {
-  const buffer = fs.readFileSync(path.join(__dirname, "users.json"))
-  const usersArray = JSON.parse(buffer.toString())
-  const user = usersArray.filter(user => user.ID === req.params.id)
+  //handler
+  const usersFilePath = path.json(__dirname, "users.json");
+  const fileAsABuffer = fs.readFileSync(usersFilePath);
+  const fileAsAString = fileAsABuffer.toString();
+  const usersArray = JSON.parse(fileAsAString)
 
-  res.send(user)
-})
+  const idComingFromRequest = req.params.id //.id is what ever you put in the url on line 31
 
+  const user = usersArray.filter(user => user.ID === idComingFromRequest);
+
+  res.send(user);
+});
+
+//3.
 router.post("/", (req, res) => {
-  const newUser = req.body
-  const buffer = fs.readFileSync(path.join(__dirname, "users.json"))
-  const content = buffer.toString()
-  const usersDB = JSON.parse(content)
-  newUser.ID = uniqid()
-  usersDB.push(newUser)
-  fs.writeFileSync(path.join(__dirname, "users.json"), JSON.stringify(usersDB))
+  //handler
 
-  res.status(201).send({ id: newUser.ID })
-})
+  //1. read the old content from the old file
+  const usersFilePath = path.json(__dirname, "users.json");
+  const fileAsABuffer = fs.readFileSync(usersFilePath);
+  const fileAsAString = fileAsABuffer.toString();
+  const usersArray = JSON.parse(fileAsAString)
 
-router.delete("/:id", (req, res) => {
-  const buffer = fs.readFileSync(path.join(__dirname, "users.json"))
-  const content = buffer.toString()
-  const usersDB = JSON.parse(content)
-  const newDb = usersDB.filter(x => x.ID !== req.params.id)
-  fs.writeFileSync(path.join(__dirname, "users.json"), JSON.stringify(newDb))
+  //2. push new user to usersArray
+  //2.b let us create a unique identifier for the new user
+  const newUser = req.body;
+  newUser.ID = uniqid();
+  usersArray.push(newUser);
 
-  res.status(204).send()
-})
+ 
+  //3 replace old contenet in the old file with new array
 
+  fs.writeFileSync(usersFilePath, JSON.stringify(usersArray));
+  res.status(201).send({ id: newUser.ID });
+});
+
+//4.  modify a single user
 router.put("/:id", (req, res) => {
-  const buffer = fs.readFileSync(path.join(__dirname, "users.json"))
-  const content = buffer.toString()
-  const usersDB = JSON.parse(content)
-  const newDb = usersDB.filter(x => x.ID !== req.params.id)
-  const users = req.body
-  users.ID = req.params.id
-  newDb.push(users)
-  fs.writeFileSync(path.join(__dirname, "users.json"), JSON.stringify(newDb))
+  //handler
 
-  res.send(newDb)
-})
+  //1. read the content again
+  const usersFilePath = path.json(__dirname, "users.json");
+  const fileAsABuffer = fs.readFileSync(usersFilePath);
+  const fileAsAString = fileAsABuffer.toString();
+  const usersArray = JSON.parse(fileAsAString)
 
-module.exports = router
+//2. Filter out the user with the specific id that you want to modify
+  const newUsersArray = usersArray.filter(user => user.ID !== req.params.id); //the ".id" equals what ever you are passing in the URL
+
+
+  //3. add the modified user back to the arry
+  const modifiedUser = req.body;
+  modifiedUser.ID = req.params.id;
+  newUsersArray.push(modifiedUser);
+
+  //4. write it back on disk
+  fs.writeFileSync(usersFilePath, JSON.stringify(usersArray));
+
+  res.send(usersArray);
+});
+
+//5.  delete a single user
+router.delete("/:id", (req, res) => {
+  //handler
+
+  //1. read the content again
+  const usersFilePath = path.json(__dirname, "users.json");
+  const fileAsABuffer = fs.readFileSync(usersFilePath);
+  const fileAsAString = fileAsABuffer.toString();
+  const usersArray = JSON.parse(fileAsAString)
+//2. filter out the user with the  specified id
+  const newUsersArray = usersArray.filter(user => user.ID !== req.params.id); //the ".id" equals what ever you are passing in the URL
+  
+  //3. save it back on the disk
+  fs.writeFileSync((usersFilePath), JSON.stringify(newUsersArray));
+
+  res.status(204).send();
+});
+
+
+
+module.exports = router; //need to export the router outside in order to use it :)
